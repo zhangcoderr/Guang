53fd4fb894f9689f1ea815ec9cde61b085c2b556
John Smith <624662670@qq.com>
2019-06-19 14:13:10 +0800

    Update Main.py
    done

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from pymouse import PyMouse
 import time
 import pyHook
 import pythoncom
-
+import xlrd
 import  pyperclip
 from pynput import mouse,keyboard
 
@@ -26,7 +26,48 @@ def copy():
 # hm.HookMouse()
 #     # 循环监听
 # pythoncom.PumpMessages()
+class ExcelData:
+    def __init__(self,keyArray,result,type):
+        self.keyArray=keyArray
+        self.result=result
+        self.type=type
 
+def getExcelData():
+    datas=[]
+    excel = xlrd.open_workbook(r"C:\Users\Administrator\Desktop\guang.xlsx")
+    table = excel.sheets()[0]
+    rowCount = table.nrows
+    colCount = table.ncols
+
+    for i in range(rowCount):
+        if(i==0): continue
+        keyArray=str(table.cell_value(i,0)).split('$')
+        result=str(table.cell_value(i,1))
+        type=str(table.cell_value(i,4))
+        data=ExcelData(keyArray,result,type)
+        datas.append(data)
+
+    return datas
+
+def getresult(str):
+    print(str)
+    datas= getExcelData()
+
+    result=''
+    contains_key=False
+    for data in datas:
+
+        for key in data.keyArray:
+            if(key in str):
+                contains_key=True
+                continue
+            else:
+                contains_key=False
+                break
+        if(contains_key):
+            result=data.result
+
+    return result
 
 def onpressed(key):
     if(key==keyboard.Key.caps_lock):
@@ -37,12 +78,20 @@ def onpressed(key):
             time.sleep(0.5)
             print('doing')
             copy()
-        if(maxTime<=0):
-            return
-        print(pyperclip.paste())
-
+        if(maxTime>0):
+            r= getresult(pyperclip.paste())
+            if(r==''):
+                print('没有这个:'+pyperclip.paste()+' ，需更新表格')
+                return
+            k.tap_key(k.escape_key)
+            k.tap_key(k.left_key)
+            k.tap_key(k.left_key)
+            k.tap_key(k.down_key)
+            k.type_string(r)
+            k.tap_key(k.enter_key)
 k=PyKeyboard()
 m=PyMouse()
+print('start')
 with keyboard.Listener(on_press=onpressed) as listener:
     listener.join()
 
